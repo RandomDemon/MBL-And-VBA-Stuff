700b848dc108e325264d1e668b5d38566a67c48e
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2022-07-27 11:09:08 -0400

    Update DiscordBotTemplate.py

--- DiscordBotTemplate.py
+++ DiscordBotTemplate.py
@@ -16,14 +16,14 @@ async def info(ctx):
 @bot.command()
 async def stats(ctx, team):
     if team == "Kobras":
-        await ctx.send("Korean Kobras: 0-0-0")
+        await ctx.send("Kobras: 0-0-0")
     elif team == "Badgers":
-        await ctx.send("Boston Badgers: 0-0-0")
+        await ctx.send("Badgers: 0-0-0")
     elif team == "Villians":
-        await ctx.send("Illest Villians: 0-0-0")
+        await ctx.send("Villians: 0-0-0")
     elif team == "Pigeons":
-        await ctx.send("New York Pigeons: 0-0-0")
+        await ctx.send("Pigeons: 0-0-0")
     else:
         await ctx.send("Invalid team name")
 
-bot.run('Blurred for obvious reasons')
+bot.run('Put your token here!')
