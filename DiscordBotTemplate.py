8217e4594db646cff97832f68bb6f83b94cba502
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-01-02 13:09:33 -0500

    Update DiscordBotTemplate.py

--- DiscordBotTemplate.py
+++ DiscordBotTemplate.py
@@ -1,29 +1,27 @@
 import discord
 from discord.ext import commands
 
-#Change the command_prefix value to anything you'd like!
 bot = commands.Bot(command_prefix='?', description="Description Here")
 
-#Checks if the bot is online
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
 async def info(ctx):
-    await ctx.send('Describe the bot here!')
+    await ctx.send('Stat Bot for the MBL Discord Server')
 
 @bot.command()
 async def stats(ctx, team):
     if team == "Kobras":
-        await ctx.send("Kobras: 0-0-0")
+        await ctx.send("Kobras: 4-2-1")
     elif team == "Badgers":
-        await ctx.send("Badgers: 0-0-0")
+        await ctx.send("Badgers: 1-5-1")
     elif team == "Villians":
-        await ctx.send("Villians: 0-0-0")
+        await ctx.send("Villians: 6-1-0")
     elif team == "Pigeons":
-        await ctx.send("Pigeons: 0-0-0")
+        await ctx.send("Pigeons: 3-4-0")
     else:
         await ctx.send("Invalid team name")
 
-bot.run('Put your token here!')
+bot.run('Put your token here')
